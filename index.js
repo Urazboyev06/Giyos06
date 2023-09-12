d0b122c9fffd2c30d62c2abc2d26f49e3a09f05f
Urazboyev06 <urazboyevg037@gmail.com>
2023-09-12 09:53:28 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//console.log(10 - "5");
+
+// let a = prompt("soni daraja")
+// let b = prompt("darajangizni korsating")
+
+// console.log(a ** b);
+
+// let a = prompt("hisoblash")
+// let b = prompt("javobi")
+
+// alert(a - b)
+
+let c = prompt("kod")
+let d = prompt("javob")
+
+alert(c * d)
